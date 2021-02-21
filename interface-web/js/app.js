9ff64357d7e553aa0837e2924623d57a6a8db467
abderrezzaq laanoui <laanoui.fst@uhp.ac.ma>
2021-02-21 15:17:01 +0100

    merging interface-web

---
+++ interface-web/js/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    var imageBoard = new DrawingBoard.Board('board',{
+    size: 10, 
+    color:'#000',
+    controls: [
+        {Size: {type: "Range"}},
+        {Navigation: { back: true, forward: false }},
+    ],
+    webStorage: true
+});
+    $("#cnvrtBtn").click(function(){
+        domtoimage.toBlob(document.getElementsByClassName('drawing-board-canvas')[0]).then(function(blob){
+            //document.getElementById('board')
+       
+        window.saveAs(blob, 'tifinagh.png');
+    })
+})
+  
+})
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
