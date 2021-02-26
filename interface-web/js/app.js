7afa3443d96795fcdca1fa3c47de3a491297a4db
abderrezzaq laanoui <laanoui.fst@uhp.ac.ma>
2021-02-26 21:32:52 +0100

    add controlers

--- interface-web/js/app.js
+++ interface-web/js/app.js
@@ -1,22 +1,3 @@
-$(document).ready(function(){
-
-    var imageBoard = new DrawingBoard.Board('board',{
-    size: 10, 
-    color:'#000',
-    controls: [
-        {Size: {type: "Range"}},
-        {Navigation: { back: true, forward: false }},
-    ],
-    webStorage: true
-});
-    $("#cnvrtBtn").click(function(){
-        domtoimage.toBlob(document.getElementsByClassName('drawing-board-canvas')[0]).then(function(blob){       
-        window.saveAs(blob, 'tifinagh.png');
-    })
-})
-  
-})
-
 function copyToClipboard(element) {
   var $temp = $("<input>");
   $("body").append($temp);
